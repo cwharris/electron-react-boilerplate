9c2cf456f5d7b070eff3604d26e835d7510d95f9
Christopher Harris <xixonia@gmail.com>
2017-02-13 21:56:02 -0600

    ts: step 1

--- server.js
+++ server.js
@@ -2,50 +2,40 @@
  * Setup and run the development server for Hot-Module-Replacement
  * https://webpack.github.io/docs/hot-module-replacement-with-webpack.html
  */
-
-import express from 'express';
-import webpack from 'webpack';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import { spawn } from 'child_process';
-
-import config from './webpack.config.development';
-
-const argv = require('minimist')(process.argv.slice(2));
-
-const app = express();
-const compiler = webpack(config);
-const PORT = process.env.PORT || 3000;
-
-const wdm = webpackDevMiddleware(compiler, {
-  publicPath: config.output.publicPath,
-  stats: {
-    colors: true
-  }
+"use strict";
+var express = require("express");
+var webpack = require("webpack");
+var webpackDevMiddleware = require("webpack-dev-middleware");
+var webpackHotMiddleware = require("webpack-hot-middleware");
+var child_process_1 = require("child_process");
+var webpack_config_development_1 = require("./webpack.config.development");
+var argv = require('minimist')(process.argv.slice(2));
+var app = express();
+var compiler = webpack(webpack_config_development_1["default"]);
+var PORT = process.env.PORT || 3000;
+var wdm = webpackDevMiddleware(compiler, {
+    publicPath: webpack_config_development_1["default"].output.publicPath,
+    stats: {
+        colors: true
+    }
 });
-
 app.use(wdm);
-
 app.use(webpackHotMiddleware(compiler));
-
-const server = app.listen(PORT, 'localhost', serverError => {
-  if (serverError) {
-    return console.error(serverError);
-  }
-
-  if (argv['start-hot']) {
-    spawn('npm', ['run', 'start-hot'], { shell: true, env: process.env, stdio: 'inherit' })
-      .on('close', code => process.exit(code))
-      .on('error', spawnError => console.error(spawnError));
-  }
-
-  console.log(`Listening at http://localhost:${PORT}`);
+var server = app.listen(PORT, 'localhost', function (serverError) {
+    if (serverError) {
+        return console.error(serverError);
+    }
+    if (argv['start-hot']) {
+        child_process_1.spawn('npm', ['run', 'start-hot'], { shell: true, env: process.env, stdio: 'inherit' })
+            .on('close', function (code) { return process.exit(code); })
+            .on('error', function (spawnError) { return console.error(spawnError); });
+    }
+    console.log("Listening at http://localhost:" + PORT);
 });
-
-process.on('SIGTERM', () => {
-  console.log('Stopping dev server');
-  wdm.close();
-  server.close(() => {
-    process.exit(0);
-  });
+process.on('SIGTERM', function () {
+    console.log('Stopping dev server');
+    wdm.close();
+    server.close(function () {
+        process.exit(0);
+    });
 });
